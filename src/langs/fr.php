9942dbe1ed879ee1dab7cb73809f04a979985a48
Le_Futuriste <spamfree@matthieubessat.fr>
2017-10-08 19:56:12 +0200

    Update fr.php

--- src/langs/fr.php
+++ src/langs/fr.php
@@ -2,14 +2,14 @@
 return [
 	'required' => "Le champs %s est requis",
 	'empty' => "Le champs %s ne peut être vide",
-	'slug' => "Le champs %s n'est pas un slug valid",
+	'slug' => "Le champs %s n'est pas un slug valide",
 	'minLength' => "Le champs %s doit contenir plus de %d caractères",
 	'maxLength' => "Le champs %s doit contenir moins de %d caractères",
 	'betweenLength' => "Le champs %s doit contenir entre %d et %d caractères",
 	'datetime' => "Le champs %s doit être une date valide (%s)",
-	'notEqual' => "Le champs %s doit être égale au champs %s",
+	'notEqual' => "Le champs %s doit être égal au champs %s",
 	'email' => "Le champs %s doit être un email valide",
 	'integer' => "Le champs %s doit être un nombre valide",
 	'url' => "Le champs %s doit être une url valide",
-	'match' => "Le champs %s doit être égale à %s"
+	'match' => "Le champs %s doit être égal à %s"
 ];
